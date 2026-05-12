db01a89ea3686a6631e1742cb11fb2275aec0584
leonardovalde <leonardovalde16@gmail.com>
2026-05-11 20:38:21 -0500

    fix: redirect back to profile after follow/unfollow

--- src/pages/api/social/follow.ts
+++ src/pages/api/social/follow.ts
@@ -4,7 +4,8 @@ import { toggleFollow } from '../../../db/queries';
 export const POST: APIRoute = async ({ request, redirect, locals }) => {
   const form = await request.formData();
   const targetId = form.get('targetId') as string;
+  const username = form.get('username') as string;
   const currentUser = locals.user!;
   if (targetId !== currentUser.id) await toggleFollow(currentUser.id, targetId);
-  return redirect(`/explore`);
+  return redirect(username ? `/u/${username}` : '/explore');
 };
